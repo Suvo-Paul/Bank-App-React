e85f97c708b1eb3e029aef35395ceb0bedd78d15
Suvo-Paul <suvopaul098@gmail.com>
2024-10-28 20:58:15 +0530

    changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] hover:scale-110 duration-300`}
+    >
+      Get Started
+    </button>
   )
 }
 
